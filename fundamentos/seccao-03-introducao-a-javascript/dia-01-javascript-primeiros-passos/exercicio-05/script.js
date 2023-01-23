a9202d558e4491c3e7e47203080356b74e7414b1
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-23 15:41:04 -0300

    exercicio 05 feito

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-05/script.js
@@ -0,0 +1,8 @@
+let weekDay = 'quarta-feira';
+
+if(weekDay === 'segunda-feira' || 'terça-feira' || 'quarta-feira' || 'quinta-feira' || 'sexta-feira') {
+    console.log('Oba, mais um dia de aprendizado na Trybe!')
+}
+else if (weekDay === 'sábado' || 'domingo') {
+    console.log('FINALMENTE, descanso merecido!')
+}
