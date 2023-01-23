7899cf368002d7062f58a849b710a0835a1a0e78
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-23 15:12:41 -0300

    exercicio if/else feito

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-03/script.js
@@ -0,0 +1,11 @@
+let valorDaNota = 78;
+
+if(valorDaNota >= 80) {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!")
+}
+else if(valorDaNota < 80 && valorDaNota >= 60) {
+    console.log("Você está na nossa lista de espera")
+}
+else if(valorDaNota < 60) {
+    console.log("Infelismente você reprovou.")
+}
--- fundamentos/seccao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++
@@ -1,7 +0,0 @@
-const base = 5;
-const height = 8;
-const area = base * height;
-const perimeter = 5 + 5 + 8 + 8;
-
-console.log(area)
-console.log(perimeter)
