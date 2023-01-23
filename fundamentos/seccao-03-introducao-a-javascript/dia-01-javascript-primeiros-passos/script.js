67a43881ac152e04d887eb8a33dcf8bebe481818
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-23 13:12:46 -0300

    exercise 2

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,9 @@
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = 5 + 5 + 8 + 8;
+
+console.log(area)
+console.log(perimeter)
+console.log(base)
+console.log(height)
