cccff831cd628a375a7d681656fe395ae55f180d
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-24 12:06:03 -0300

    exercicio 1 feito

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-02-javascript-array-for/exercicio-01/script.js
@@ -0,0 +1,13 @@
+// 1- Obtenha o valor “Serviços” do array menu:
+
+// 2- Procure o índice do valor “Portfólio” do array menu:
+
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexPortfólio = menu.indexOf('Portfólio');
+
+
+console.log(menu[1])
+console.log(menu.indexOf('Portfólio'))
+
+// Para saber o index do portfólio é necessário criar uma variavel definindo o que queremos e usar o (menu.indexOf()).
