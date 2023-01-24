1573d895061cbaa920a116151a57d7b21f24a892
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-24 15:41:38 -0300

    exercicio 6 modificado

--- fundamentos/seccao-03-introducao-a-javascript/dia-02-javascript-array-for/exercicio-06/script.js
+++ fundamentos/seccao-03-introducao-a-javascript/dia-02-javascript-array-for/exercicio-06/script.js
@@ -7,5 +7,17 @@ for (let index = 0; index < numbers.length; index += 1) {
     soma += numbers[index]
 }
 
-let media = soma / numbers.length
-console.log(media)
+// let media = soma / numbers.length
+// console.log(media)
+
+soma = soma / numbers.length
+
+
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, 
+// imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+if (soma > 20) {
+    console.log("valor maior que 20")
+}   else {
+    console.log("valor menor ou igual a 20")
+}
