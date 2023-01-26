b71639ff08b36ce8d8bc79a39ec4d45de3f8d9b7
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-26 11:04:42 -0300

    exercicio 5 feito

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-04-javascript-objetos-funcoes/exercicio-05/script.js
@@ -0,0 +1,10 @@
+// Faça um programa que retorne o maior de dois números. 
+// Defina, no começo do programa, duas constantes com os valores que serão comparados.
+
+function maior (a, b) {
+    if (a > b) {
+        return 'a é maior!'
+    }   else {
+        return 'b é maior'
+    }
+};
