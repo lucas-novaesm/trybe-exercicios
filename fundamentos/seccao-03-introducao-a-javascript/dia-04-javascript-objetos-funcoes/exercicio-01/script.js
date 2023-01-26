2141cde71bf53adb64a88c2489af48d32899980c
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-26 08:53:22 -0300

    exercicio 1 feito

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-04-javascript-objetos-funcoes/exercicio-01/script.js
@@ -0,0 +1,15 @@
+// Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let player = {
+    nome: 'Marta',
+    sobrenome: 'Silva',
+    idade: 34,
+    medals: ['golden', 'silver']
+}
+
+console.log(player)
