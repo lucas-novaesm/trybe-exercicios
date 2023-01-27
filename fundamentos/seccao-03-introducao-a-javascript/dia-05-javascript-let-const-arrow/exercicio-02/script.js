a9d777b53799b908d274968db40a55d3e5cffe39
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-27 18:27:50 -0300

    exercicio 2 feito

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-05-javascript-let-const-arrow/exercicio-02/script.js
@@ -0,0 +1,10 @@
+// 🚀 Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const;
+
+  const pessoa = {
+    nome: 'Henri',
+    idade: 20
+  }
+  pessoa.nome = 'Luna'
+  pessoa.idade = '19'
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
