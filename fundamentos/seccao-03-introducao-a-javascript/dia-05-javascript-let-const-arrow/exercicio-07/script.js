45d32f682ad9349219588aaf5c689403c78f848c
lucas-novaesm <luc.novaesm@gmail.com>
2023-01-27 18:43:58 -0300

    exercicio 7 feito

---
+++ fundamentos/seccao-03-introducao-a-javascript/dia-05-javascript-let-const-arrow/exercicio-07/script.js
@@ -0,0 +1,9 @@
+// 🚀 Transforme a função nomeCompleto em uma arrow function;
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`
+  }
+  
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome));
